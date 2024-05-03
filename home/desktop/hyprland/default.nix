f1ca839608fd09a30a8553a9c0d8de5abd784b28
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-03 15:10:56 +0200

    home-manager switch: ven 3 mag 2024, 15:10:56, CEST

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -8,46 +8,6 @@
 
   wayland.windowManager.hyprland.enable = true;
   
-  imports = [
-    # ./config.nix 
-    # ./rules.nix
-  ];
-  
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    bind =
-      [
-        "$mod, F, exec, firefox"
-        ", Print, exec, grimblast copy area"
-        "$mod,MINUS,killactive"
-        "$mod, Q, exec, kitty"
-        "$mod, F, exec, firefox"
-
-        "$mod,H,movefocus,l"
-        "$mod,L,movefocus,r"
-        "$mod,K,movefocus,u"
-        "$mod,J,movefocus,d"
-
-        "$mod,V,togglefloating," # toggle floating for the focused window
-        "$mod,up,fullscreen," # fullscreen focused window
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-        builtins.concatLists (builtins.genList (
-            x: let
-              ws = let
-                c = (x + 1) / 10;
-              in
-                builtins.toString (x + 1 - (c * 10));
-            in [
-              "$mod, ${ws}, workspace, ${toString (x + 1)}"
-              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-            ]
-          )
-          10)
-      );
-  };
   
   # fake a tray to let apps start
   # https://github.com/nix-community/home-manager/issues/2064
