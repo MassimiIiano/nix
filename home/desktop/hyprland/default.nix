be7010670d9f5279bf8b30beca2a1e7f87eb92ad
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-03 15:56:02 +0200

    home-manager switch: ven 3 mag 2024, 15:56:02, CEST

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -8,5 +8,6 @@
 
   # wayland.windowManager.hyprland.enable = true;
   home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf; 
+  home.file.".config/hypr/start.sh".source = ./start.sh;
   
 }
