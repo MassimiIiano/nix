b0253f20fe3c1be742c2c10709b7288d234f88c1
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-03 12:50:53 +0200

    home-manager switch: ven 3 mag 2024, 12:50:53, CEST

---
+++ switch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Add all files to the staging area
+git add .
+
+# Commit the changes with the current date and time as the message
+git commit -m "home-manager switch: $(date)"
+
+# Push the changes to the remote repository
+git push
+
+# Rebuld NixOS
+sudo home-manager switch
