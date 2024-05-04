81551ba59b8550c627f91e598fbc748141a26ebc
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-04 02:46:45 +0200

    home-manager switch:

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -6,7 +6,7 @@
   ...
 }:{
 
-  home.packages = with pkgs; [ gnome.file-roller ];
+  home.packages = with pkgs; [ gnome.file-roller hyprlock ];
   home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf; 
   home.file.".config/hypr/start.sh".source = ./start.sh;
   
