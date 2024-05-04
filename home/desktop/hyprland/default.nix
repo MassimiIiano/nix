4dbe2620f31daf3b2d7cf17f35f52a49794de93b
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-04 11:59:06 +0200

    home-manager switch: hyprland to default

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -7,7 +7,7 @@
 }:{
 
   home.packages = with pkgs; [ gnome.file-roller hyprlock ];
-  wayland.windowManager.hyprland.enable = true;
-  # home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf; 
-  # home.file.".config/hypr/start.sh".source = ./start.sh;
+  # wayland.windowManager.hyprland.enable = true;
+  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf; 
+  home.file.".config/hypr/start.sh".source = ./start.sh;
 }
