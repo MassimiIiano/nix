12b3385e87295e31fbc3bfbd3e530f9acd7ad57f
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-06 23:14:52 +0200

    home-manager switch:

--- home/desktop/waybar/default.nix
+++ home/desktop/waybar/default.nix
@@ -1,7 +1,7 @@
 # This Nix expression defines the configuration for the Waybar status bar.
 { pkgs, lib, ... }:
 {
-  Configure the Waybar program.
+  # Configure the Waybar program.
   programs.waybar = {
     enable = true;
     style = import ./style.nix;
