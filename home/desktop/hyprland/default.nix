8610a72aba2407505defadd8e1396335b60004d7
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-07 11:31:35 +0200

    home-manager switch: added xwaylad to in hypr

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -6,6 +6,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     systemd.enable = true;
+    xwayland.enable = true;
     extraConfig = 
 ''
 # #######################################################################################
