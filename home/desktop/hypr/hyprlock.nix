c3a4f6ac86ae48ed3d3f7aea8cc9a2d026987b57
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-07 13:39:51 +0200

    home-manager switch: somthing even more stupid

--- home/desktop/hypr/hyprlock.nix
+++ home/desktop/hypr/hyprlock.nix
@@ -2,7 +2,7 @@ _ :
 {
   programs.hyprlock = {
     enable = true;  
-    sources = [ ./hyprlock.conf ];  
+    sources = ./hyprlock.conf;  
   };
   # programs.hyprlock = {
   # home.file.".config/hypr/hyprlock.conf".source = ./hyprlock.conf;
