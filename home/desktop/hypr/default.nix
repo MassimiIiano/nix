ad57498231936ffc76ea9ba2f84e29361898bc55
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-11 15:02:21 +0200

    home-manager switch: test

--- home/desktop/hypr/default.nix
+++ home/desktop/hypr/default.nix
@@ -30,7 +30,7 @@
 
 # See https://wiki.hyprland.org/Configuring/Monitors/
 
-monitor=,preferred,auto,1.5
+monitor=,preferred,auto,auto
 
 # See https://wiki.hyprland.org/Configuring/Keywords/ for more
 
@@ -211,8 +211,7 @@ bind = ,XF86AudioLowerVolume, exec ,amixer set Master 5%-
 bind = ,XF86MonBrightnessUp, exec, brightnessctl set 10%+
 bind = ,XF86MonBrightnessDown, exec, brightnessctl set 10%-
 
-exec = pkill waybar & sleep 0.5 && waybar
-exec-once = swww-daemon & dunst & images=(~/Pictures/backgrounds/*); while true; do for img in "''${images[@]}"; do swww img "$img" & sleep 10; done; done
+exec = pkill waybar & sleep 0.5 && waybar & swww-daemon & dunst & images=(~/Pictures/backgrounds/*); while true; do for img in "''${images[@]}"; do swww img "$img" & sleep 10; done; done
 '';
   };
 
