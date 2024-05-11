4e31b88245341fc8795529ca4e92eedd34c9fc30
MassimiIano <massimiliano.mola.bzs@gmail.com>
2024-05-11 10:28:10 +0200

    home-manager switch: changing

--- home/desktop/hypr/start.sh
+++ home/desktop/hypr/start.sh
@@ -3,8 +3,18 @@
 # initialize wallpaper daemon
 swww-daemon &
 
-# set wallpaper
-swww img ~/Pictures/backgrounds/confy-distopia.gif &
-
 # notification daemon
-dunst 
+dunst &
+
+# set wallpaper
+images=(~/Pictures/backgrounds/*)
+
+# loop over the images
+while true; do
+  for img in "${images[@]}"; do
+    # set wallpaper
+    swww img "$img" &
+    # wait for x seconds
+    sleep 10
+  done
+done
